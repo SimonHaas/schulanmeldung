aee198125fd9ba96bed880f22de0975f152a69bf
Simon <simon-ha@t-online.de>
2019-02-20 11:01:22 +0100

    crud for Ausbildung und Beruf

--- src/Form/AusbildungType.php
+++ src/Form/AusbildungType.php
@@ -2,23 +2,28 @@
 
 namespace App\Form;
 
-use App\Entity\Beruf;
+use App\Entity\Ausbildung;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class BerufType extends AbstractType
+class AusbildungType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
+            ->add('beginn')
+            ->add('ende')
+            ->add('relation')
+            ->add('betrieb')
+            ->add('betriebId')
         ;
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
-            'data_class' => Beruf::class,
+            'data_class' => Ausbildung::class,
         ]);
     }
 }
