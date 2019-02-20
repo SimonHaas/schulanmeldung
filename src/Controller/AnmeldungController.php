b354d99f83bddd1bdcc5bffcf45aefbc892e85fd
Simon <simon-ha@t-online.de>
2019-02-20 14:19:11 +0100

    save registrierung in session

--- src/Controller/AnmeldungController.php
+++ src/Controller/AnmeldungController.php
@@ -2,26 +2,39 @@
 
 namespace App\Controller;
 
+use App\Entity\Registrierung;
+use DateTime;
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AnmeldungController extends AbstractController
 {
     /**
      * @Route("/anmeldung", name="anmeldung")
+     * @throws Exception
      */
-    public function index()
+    public function index(Request $request)
     {
+        $session = new Session();
+
+        $registrierung = new Registrierung();
+        $registrierung->setIp($request->getClientIp());
+        $registrierung->setDatum(new DateTime());
+        $session->set('registrierung', $registrierung);
+
         return $this->render('anmeldung/index.html.twig');
     }
 
     /**
-     * @Route("/daten-pruefen", name="daten_pruefen")
-     * @param Request $request
-     * @return \Symfony\Component\HttpFoundation\Response
+     * @Route("/daten-pruefen/{Registrierung}", name="daten_pruefen")
+     * @param Registrierung $registrierung
+     * @return Response
      */
-    public function check(Request $request)
+    public function check(Registrierung $registrierung)
     {
         //TODO daten zusammensammeln und ans Template weitergeben
 
