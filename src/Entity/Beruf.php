0872d3012bb69ef93ee582397d10798ac2189e8b
Max <coding@maax.gr>
2019-02-20 09:22:33 +0100

    table annotations

--- src/Entity/Beruf.php
+++ src/Entity/Beruf.php
@@ -7,6 +7,7 @@ use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
+ * @ORM\Table(name="berufsdaten")
  * @ORM\Entity(repositoryClass="App\Repository\BerufRepository")
  */
 class Beruf
