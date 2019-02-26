cc8c1f956db5d4a0e2e9ab6bcb2d93e33d6bc84e
Max <coding@maax.gr>
2019-02-26 10:18:21 +0100

    export template erweitert

--- src/Controller/AdminRegistrationsControllers.php
+++ src/Controller/AdminRegistrationsControllers.php
@@ -6,19 +6,28 @@ use App\Entity\Registrierung;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
-class AdminRegistrationController extends AbstractController
+class AdminRegistrationsControllers extends AbstractController
 {
     /**
-     * @Route("/admin/registration", name="admin_registration")
+     * @Route("/admin/registrations", name="admin_registrations")
      */
     public function index()
     {
 
         $registrations = $this->getDoctrine()->getRepository(Registrierung::class)->findAll();
 
-        return $this->render('admin_registration/index.html.twig', [
+        return $this->render('admin_registrations/index.html.twig', [
             'controller_name' => 'AdminRegistrationController',
             'registrations' => $registrations
         ]);
     }
+
+    /**
+     * @Route("/admin/registrations/delete/{id}", name="admin_registrations_delete")
+     */
+    public function delete($id) {
+        exit($id);
+        return $this->redirectToRoute('admin');
+    }
+
 }
